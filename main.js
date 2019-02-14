fc874e98db201aed04fe1ea5d07f096897f0354b
jeppjob <jepp.job@gmail.com>
2019-02-15 01:47:36 +0800

    added animation

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
-document.addEventListener("swup:pageView",function(){
-//Menu
-let navbarLinks = document.querySelector(".navbar-links");
-let navbarSocial = document.querySelector(".navbar-social");
+document.addEventListener("swup:pageView", function () {
+    //Menu
+    let navbarLinks = document.querySelector(".navbar-links");
+    let navbarSocial = document.querySelector(".navbar-social");
 
 
-//Contact
-let contactDiv = document.querySelector(".contact");
-//Links and Buttons
-let menu = document.getElementById("menu");
-let contact = document.querySelector(".contact-btn");
-let close = document.querySelector(".close");
-//Wrapper
-let wrapper = document.getElementById("wrapper");
+    //Contact
+    let contactDiv = document.querySelector(".contact");
+    //Links and Buttons
+    let menu = document.getElementById("menu");
+    let contact = document.querySelectorAll(".contact-btn");
+    let close = document.querySelector(".close");
+    //Wrapper
+    let wrapper = document.getElementById("wrapper");
 
 
-//functions
-menu.addEventListener("click",function(){
-    if(navbarLinks.classList){
-        navbarLinks.classList.toggle("pop-in");
-        navbarSocial.classList.toggle("pop-out");
+    //functions
+    menu.addEventListener("click", function () {
+        if (navbarLinks.classList) {
+            navbarLinks.classList.toggle("pop-in");
+            navbarSocial.classList.toggle("pop-out");
+        }
+    });
+
+    for (i = 0; i < contact.length; i++) {
+        contact[i].addEventListener("click", function () {
+            if (contactDiv.classList) {
+                contactDiv.classList.toggle("contact-in");
+                wrapper.classList.toggle("blur");
+            }
+        });
     }
-});
 
-contact.addEventListener("click",function(){
-    if(contactDiv.classList){
-        contactDiv.classList.toggle("contact-in");
-        wrapper.classList.toggle("blur");
-    }
-});
 
-close.addEventListener("click",function(){
-    if(contactDiv.classList){
-        contactDiv.classList.toggle("contact-in");
-        wrapper.classList.toggle("blur");
-    }
-});
+    close.addEventListener("click", function () {
+        if (contactDiv.classList) {
+            contactDiv.classList.toggle("contact-in");
+            wrapper.classList.toggle("blur");
+        }
+    });
 });
